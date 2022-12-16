bd6962f1e5c4d08ccb0837d290f60479937d086c
ImSAMazing <sam@liemburg.nl>
2022-12-16 22:46:11 +0100

    Het leeft!!!!!

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,17 +1,19 @@
+use axum::body::{boxed, Body};
+use axum::http::{Response, StatusCode};
 use axum::{response::IntoResponse, routing::get, Router};
 use clap::Parser;
 use std::net::{IpAddr, Ipv6Addr, SocketAddr};
 use std::str::FromStr;
-use tower::{ServiceBuilder, Service};
+use tower::{ServiceBuilder, ServiceExt};
+use tower_http::services::ServeDir;
 use tower_http::trace::TraceLayer;
 
 // Setup the command line interface with clap.
 #[derive(Parser, Debug)]
 #[clap(name = "server", about = "A server for our wasm project!")]
 struct Opt {
-
     ///set the log level
-    #[clap(short='l', long="log", default_value="debug")]
+    #[clap(short = 'l', long = "log", default_value = "debug")]
     log_level: String,
 
     /// set the listen addr
@@ -21,6 +23,9 @@ struct Opt {
     /// set the listen port
     #[clap(short = 'p', long = "port", default_value = "8080")]
     port: u16,
+    /// set the directory where static files are to be found
+    #[clap(long = "static-dir", default_value = "./dist")]
+    static_dir: String,
 }
 
 #[tokio::main]
@@ -28,13 +33,19 @@ async fn main() {
     let opt = Opt::parse();
 
     //Setup logging & RUST_LOG from args
-    if std::env::var("RUST_LOG").is_err(){
+    if std::env::var("RUST_LOG").is_err() {
         std::env::set_var("RUST_LOG", format!("{},hyper=info,mio=info", opt.log_level));
     }
     //enable console logging
     tracing_subscriber::fmt::init();
 
-    let app = Router::new().route("/", get(hello)).layer(ServiceBuilder::new().layer(TraceLayer::new_for_http()));
+    let app = Router::new()
+        .route("/api/hello", get(hello))
+        .merge(axum_extra::routing::SpaRouter::new(
+            "/assets",
+            opt.static_dir,
+        ))
+        .layer(ServiceBuilder::new().layer(TraceLayer::new_for_http()));
 
     let sock_addr = SocketAddr::from((
         IpAddr::from_str(opt.addr.as_str()).unwrap_or(IpAddr::V6(Ipv6Addr::LOCALHOST)),
