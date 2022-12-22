79151403503cd788c3f0215d56eea452a177d76f
ImSAMazing <sam@liemburg.nl>
2022-12-22 22:30:20 +0100

    Opzet voor jwt decoden

--- server/src/autentisering/mod.rs
+++ server/src/autentisering/mod.rs
@@ -13,6 +13,10 @@ struct Claims {
     exp: usize,
 }
 
+pub struct Jwt_Information {
+    hjärnannamn: String,
+}
+
 impl Claims {
     fn skaffa_mig_audience() -> String {
         "HjärnorFörening".to_string()
@@ -23,10 +27,10 @@ impl Claims {
     fn skaffa_mig_tidsspan() -> i64 {
         7 * 24 * 60 * 60
     }
-    fn producera(subject: String) -> Claims {
+    fn producera(information: Jwt_Information) -> Claims {
         let now = Utc::now().timestamp();
         Claims {
-            sub: subject,
+            sub: information.hjärnannamn,
             aud: Claims::skaffa_mig_audience(),
             iss: Claims::skaffa_mig_issuer(),
             iat: now as usize,
@@ -42,8 +46,28 @@ fn skaffa_mig_din_hemlighet() -> EncodingKey {
             .as_bytes(),
     )
 }
+
+fn skaffa_mig_din_hemlighet_decoding() -> DecodingKey {
+    DecodingKey::from_secret("secret".as_ref())
+}
+
 pub fn producera_jwt(hjärnannamn: String) -> String {
-    let claims = Claims::producera(hjärnannamn);
+    producera_jwt_från_information(Jwt_Information { hjärnannamn })
+}
+
+fn producera_jwt_från_information(information: Jwt_Information) -> String {
+    let claims = Claims::producera(information);
     let token = encode(&Header::default(), &claims, &skaffa_mig_din_hemlighet());
     token.unwrap()
+}
+
+pub fn konvertera_jwt(raw_token: &str) -> Jwt_Information {
+    let mut validator = Validation::default();
+    validator.set_audience(Claims::skaffa_mig_audience().as_bytes());
+    validator.set_issuer(Claims::skaffa_mig_issuer().as_bytes());
+    let token =
+        decode::<Claims>(raw_token, &skaffa_mig_din_hemlighet_decoding(), &validator).unwrap();
+    Jwt_Information {
+        hjärnannamn: token.claims.sub,
+    }
 }
