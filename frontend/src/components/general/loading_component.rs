7ed7b3f09129937caa6e559736fe0c6d39e1bd3e
ImSAMazing <sam@liemburg.nl>
2023-01-14 08:43:09 +0100

    Kleinee ergernis weg

--- frontend/src/components/general/loading_component.rs
+++ frontend/src/components/general/loading_component.rs
@@ -24,7 +24,7 @@ impl Component for LoadingComponent {
         html! {
         <div class="flex justify-center items-center">
             <div class="spinner-border animate-spin inline-block w-8 h-8 border-4 rounded-full" role="status">
-              <span class="visually-hidden">{"Loading..."}</span>
+              <span class="visually-hidden">{"Loading"}</span>
             </div>
         </div>
         }
